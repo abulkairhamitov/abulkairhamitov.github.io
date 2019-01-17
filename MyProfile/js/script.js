de24f7d32338c7fdc3343effae25f9c3ba507a31
abulkairhamitov <46451501+abulkairhamitov@users.noreply.github.com>
2019-01-17 23:10:20 +0600

    commit to master

---
+++ MyProfile/js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+	$(".fa-bars").click(function(){
+		$(".sidebar-menu").addClass("hide-menu");
+		$(".toggle-menu").addClass("opacity-zero");
+	});
+	$(".fa-times").click(function(){
+		$(".sidebar-menu").removeClass("hide-menu");
+		$(".toggle-menu").removeClass("opacity-zero");
+	});
+});
+
+  var loader;
+        function loadNow(opacity) {
+            if(opacity <= 0) {
+                displayContent();
+            }
+            else {
+                loader.style.opacity = opacity;
+                window.setTimeout(function() {
+                    loadNow(opacity - 0.05)
+                }, 100);
+            }
+        }
+        
+        function displayContent() {
+            loader.style.display = 'none';
+            document.getElementById('content').style.display = 'block';
+        }
+        
+        document.addEventListener("DOMContentLoaded", function() {
+            loader = document.getElementById('loader');
+            loadNow(1);
+        });
+
+     
+      
+    
