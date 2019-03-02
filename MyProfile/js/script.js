631ddcf88c848ebb43664fc94a4cf6862794fd0d
abulkairhamitov <46451501+abulkairhamitov@users.noreply.github.com>
2019-03-02 19:40:59 +0600

    commit to master

--- MyProfile/js/script.js
+++ MyProfile/js/script.js
@@ -1,37 +1,6 @@
-$(document).ready(function() {
-	$(".fa-bars").click(function(){
-		$(".sidebar-menu").addClass("hide-menu");
-		$(".toggle-menu").addClass("opacity-zero");
-	});
-	$(".fa-times").click(function(){
-		$(".sidebar-menu").removeClass("hide-menu");
-		$(".toggle-menu").removeClass("opacity-zero");
-	});
+$('h3').click(function() {
+    $('nav').addClass('menu menu_active');
 });
-
-  var loader;
-        function loadNow(opacity) {
-            if(opacity <= 0) {
-                displayContent();
-            }
-            else {
-                loader.style.opacity = opacity;
-                window.setTimeout(function() {
-                    loadNow(opacity - 0.05)
-                }, 100);
-            }
-        }
-        
-        function displayContent() {
-            loader.style.display = 'none';
-            document.getElementById('content').style.display = 'block';
-        }
-        
-        document.addEventListener("DOMContentLoaded", function() {
-            loader = document.getElementById('loader');
-            loadNow(1);
-        });
-
- 
-      
-    
+$('p').click(function() {
+    $('nav').removeClass('menu menu_active');
+});
