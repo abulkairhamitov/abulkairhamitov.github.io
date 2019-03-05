502ade94afddec66fac66a3471488146a91ced92
abulkairhamitov <46451501+abulkairhamitov@users.noreply.github.com>
2019-03-05 21:35:24 +0600

    commit to master

--- MyProfile/js/script.js
+++ MyProfile/js/script.js
@@ -4,3 +4,10 @@ $('h3').click(function() {
 $('p').click(function() {
     $('nav').removeClass('menu menu_active');
 });
+
+$('h2').click(function() {
+    $('.folders_container').addClass('folders_container_active');
+});
+$('p').click(function() {
+    $('nav').removeClass('menu menu_active');
+});
