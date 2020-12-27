ff4fc7a4adf28bb7f448a324039a9bde6b3026bc
abulkairhamitov <46451501+abulkairhamitov@users.noreply.github.com>
2020-12-27 17:40:23 +0600

    alpha_test

--- tamagotchi/script.js
+++ tamagotchi/script.js
@@ -1,31 +1,5 @@
-// function face() {
-// 	// let face = document.getElementsByClassName("animate_face");
-// 	// for( let i = 0; i < face.length; i++){ 
-// 	// 	face[i].style.animation = "husky-tongue 7s none infinite";
-// 	// }
-// 	let tongue = document.getElementsByClassName("husky-tongue");
-// 	let mouth = document.getElementsByClassName("husky-mouth");
-
-// 	mouth[0].classList.add('mouth');
-// 	tongue[0].style.animation = "husky-tongue 4s none infinite";
-// 	function remove() {
-// 		let tongue = document.getElementsByClassName("husky-tongue");
-// 		let mouth = document.getElementsByClassName("husky-mouth");
-// 		tongue[0].style.animation = "none";
-// 		mouth[0].classList.remove('mouth');
-// 	}
-// 	setTimeout(remove, 4000);
-// }
-
-
-// 
-
 const tamagotchi = {
     state: 'l0',
-
-    sleep() {
-    	console.log("remove");
-    },
 
     dispatch(actionName) {   // actionName - входные данные
         const action = this.transitions[this.state][actionName];    // Достаем метод текущего обьекта (состояния)
@@ -38,7 +12,6 @@ const tamagotchi = {
     changeStateTo(newState) {
         this.state = newState;
     },
-
 
     eye: document.getElementsByClassName("husky-eye"),			//(1)
     f_eye() {
@@ -93,64 +66,61 @@ const tamagotchi = {
 
     remove_all() {
 		if(document.querySelector('.husky-eye.eye')) {
-			let eye = document.getElementsByClassName("husky-eye");
-    		eye[0].classList.remove('eye');
-			eye[1].classList.remove('eye');
+    		this.eye[0].classList.remove('eye');
+			this.eye[1].classList.remove('eye');
 		}
 
 		if(document.querySelector('.husky-ear.ear')) {
-			let ear = document.getElementsByClassName("husky-ear");
-			ear[0].classList.remove('ear');
-			ear[1].classList.remove('ear');
+			this.ear[0].classList.remove('ear');
+			this.ear[1].classList.remove('ear');
 		}
 
     	if(document.querySelector('.husky-head.head')) {
-		    body = document.getElementsByClassName("husky-body");		//(4)
-		    mane = document.getElementsByClassName("husky-mane");
-		    face = document.getElementsByClassName("husky-face");
-		    head = document.getElementsByClassName("husky-head");
-		    nose = document.getElementsByClassName("husky-nose");
-			head[0].classList.remove('head');
-			mane[0].classList.remove('mane');
-			face[0].classList.remove('face');
-			body[0].classList.remove('body');
-			nose[0].classList.remove('nose');
+			this.head[0].classList.remove('head');
+			this.mane[0].classList.remove('mane');
+			this.face[0].classList.remove('face');
+			this.body[0].classList.remove('body');
+			this.nose[0].classList.remove('nose');
     	}
 
 
 		if(document.querySelector('.husky-tail.tail')) {
-			let tail = document.getElementsByClassName("husky-tail");
 			for( let i = 1; i < this.tail.length; i++) { 
-				tail[i].classList.remove('tail');
+				this.tail[i].classList.remove('tail');
 			}
 		}
 
 		if(document.querySelector('.husky-mouth.mouth')) {
-			let mouth = document.getElementsByClassName("husky-mouth");
-			mouth[0].classList.remove('mouth');
+			this.mouth[0].classList.remove('mouth');
 		}
 
 		if(document.querySelector('.husky-tongue.tongue')) {
-			let tongue = document.getElementsByClassName("husky-tongue");
-			tongue[0].classList.remove('tongue');
+			this.tongue[0].classList.remove('tongue');
 		}
     },
+
+    counter(i) {
+    	document.getElementById("counter").innerHTML= i;
+	},
     // li - i-ое состояния
     transitions: {
         'l0': {
             data_in1: function() {	// (1) Глаза
             	this.f_eye();
+            	this.counter(1);
                 this.changeStateTo('l1');
             },
             data_in2: function() {	// (1) (2) лУшко
             	this.f_eye();
             	this.f_ear1();
+            	this.counter(2);
                 this.changeStateTo('l2');
             },
             data_in3: function() {	// (1) (2) (3) (4) Голова
             	this.f_eye();
             	this.f_ear2();
             	this.f_head();
+            	this.counter(4);
                 this.changeStateTo('l4');
             },
         },
@@ -161,6 +131,7 @@ const tamagotchi = {
             data_in2: function() { 	// (1) (2) (3) пУшко
             	this.f_eye();
             	this.f_ear2();
+            	this.counter(3);
                 this.changeStateTo('l3');
             },
             data_in3: function() {	// (1) (2) (3) (4) (5) Хвост
@@ -168,6 +139,7 @@ const tamagotchi = {
             	this.f_ear2();
             	this.f_head();
             	this.f_tail();
+            	this.counter(5);
                 this.changeStateTo('l5');
             },
         },
@@ -175,6 +147,7 @@ const tamagotchi = {
             data_in1: function() {	// (1) (2) (3) пУшко
             	this.f_eye();
             	this.f_ear2();
+            	this.counter(3);
                 this.changeStateTo('l3');
             },
             data_in2: function() {	// Ничего
@@ -186,6 +159,7 @@ const tamagotchi = {
             	this.f_head();
             	this.f_tail();
             	this.f_mouth();
+            	this.counter(6);
                 this.changeStateTo('l6');
             },
         },
@@ -202,7 +176,8 @@ const tamagotchi = {
             	this.f_head();
             	this.f_tail();
             	this.f_mouth();
-            	this.f_tongue();
+            	this.f_tongue()
+            	this.counter(7);
                 this.changeStateTo('l7');
             },
         },
@@ -212,6 +187,7 @@ const tamagotchi = {
             	this.f_ear2();
             	this.f_head();
             	this.f_tail();
+            	this.counter(5);
                 this.changeStateTo('l5');
             },
             data_in2: function() {	// (1) (2) (3) (4) (5) (6) Рот
@@ -220,6 +196,7 @@ const tamagotchi = {
             	this.f_head();
             	this.f_tail();
             	this.f_mouth();
+            	this.counter(6);
                 this.changeStateTo('l6');
             },
             data_in3: function() {	// Ничего
@@ -237,6 +214,7 @@ const tamagotchi = {
             	this.f_tail();
             	this.f_mouth();
             	this.f_tongue();
+            	this.counter(7);
                 this.changeStateTo('l7');
             },
             data_in3: function() {	// Ничего
@@ -251,6 +229,7 @@ const tamagotchi = {
             	this.f_tail();
             	this.f_mouth();
             	this.f_tongue();
+            	this.counter(7);
                 this.changeStateTo('l7');
             },
             data_in2: function() {	// Ничего
@@ -263,6 +242,7 @@ const tamagotchi = {
             	this.f_tail();
             	this.f_mouth();
             	this.f_tongue();
+            	this.counter(7);
                 this.changeStateTo('l7');
             },
         },
@@ -270,31 +250,31 @@ const tamagotchi = {
         	// Удаление всех анимаций, переход на начальное состояние
             data_in1: function() {
             	this.remove_all();
+            	this.counter(0);
                 this.changeStateTo('l0');
             },
             data_in2: function() {
             	this.remove_all();
+            	this.counter(0);
                 this.changeStateTo('l0');
             },
             data_in3: function() {
             	this.remove_all();
+            	this.counter(0);
                 this.changeStateTo('l0');
             },
         },
     }
 }
 
-function data_in1() {
-    tamagotchi.dispatch('data_in1');
-}
+var buttons = document.getElementsByClassName("button");
 
-function data_in2() {
-    tamagotchi.dispatch('data_in2');
+buttons[0].onclick = function() {
+	tamagotchi.dispatch("data_in1");
 }
-
-function data_in3() {
-    tamagotchi.dispatch('data_in3');
+buttons[1].onclick = function() {
+	tamagotchi.dispatch("data_in2");
+} 
+buttons[2].onclick = function() {
+	tamagotchi.dispatch("data_in3");
 }
-
-// Функция dispatch - для проверки существования такого состояния
-// Функция changeStateTo - для изменения состояния
